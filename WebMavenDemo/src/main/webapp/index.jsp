b9974cddc28951e11450e8972dba24ca148d1a9c
dongdian-git <18702994997@163.com>
2020-05-31 11:22:12 +0800

    第一次提交到本地库

---
+++ WebMavenDemo/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<body>
+<h2 style="color: #7ee6ff">Hello World!这是我在idea上的第一个maven  web项目！</h2>
+</body>
+</html>
---
+++ src/main/java/com/hirisun/utils/test.java
@@ -0,0 +1,13 @@
+package com.hirisun.utils;
+
+/**
+ * @Auther: dongdian
+ * @Date: 2020/5/31 - 05 - 31 - 10:22
+ * @Description: com.hirisun.utils
+ * @version: 1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
